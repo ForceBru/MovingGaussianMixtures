ed735b00204ffb1fc3682f3ffb60eba69c6d0641
ForceBru <ForceBru@users.noreply.github.com>
2021-04-25 19:58:50 +0300

    Now user can pass their own plotting params

--- src/Plotting.jl
+++ src/Plotting.jl
@@ -1,26 +1,35 @@
 "Plot kernel density estimate"
 function Plots.plot(
-	est::GaussianMixtureEstimate{k, T}, x_orig::Union{Missing, AbstractVector}=missing;
-	n_sigmas::Real=3, x_length=500
-) where {k, T <: Real}
-	x_lo, x_hi = let
+	est::GaussianMixtureEstimate{k}, x_orig::Union{Missing, AbstractVector}=missing;
+	n_sigmas::Real=3, x_length::Integer=500,
+	title::Union{Missing, AbstractString}=missing, alpha::Real=.5, ylims=(0.0, Inf)
+) where k
+	@assert 0 <= alpha <= 1
+
+	x_lo, x_hi = if x_orig === missing
 		idx_lo, idx_hi = argmin(est.μ), argmax(est.μ)
 
 		est.μ[idx_lo] - n_sigmas * est.σ[idx_lo], est.μ[idx_hi] + n_sigmas * est.σ[idx_hi]
+	else
+		minimum(x_orig), maximum(x_orig)
 	end
 
 	x = range(x_lo, x_hi; length=x_length)
 	kde = zeros(size(x))
 	est_sorted = sort(est, by=:p)
 
-	title = "Gaussian mixture with $(Int(k)) components"
+	if title === missing
+		title = "Gaussian mixture with $(Int(k)) components"
+	end
+
 	plt = if x_orig === missing
 		Plots.plot(title=title)
 	else
 		Plots.histogram(
 			x_orig, normalize=:pdf,
 			title=title, label="Original data",
-			linewidth=0, alpha=.5
+			linewidth=0, alpha=alpha,
+			fontfamily="monospace"
 		)
 	end
 
@@ -29,29 +38,35 @@ function Plots.plot(
 		pdf_ = p .* pdf(x, μ, σ)
 		kde .+= pdf_
 
-		label = i <= 5 ? @sprintf("p=%.2f μ=%+.3f σ=%.3f", p, μ, σ) : ""
+		label = i <= 5 ? @sprintf("p=%.2f μ=% .3f σ=%.3f", p, μ, σ) : ""
 
-		Plots.plot!(plt, x, pdf_, label=label, ylims=(0.0, Inf))
+		Plots.plot!(plt, x, pdf_, label=label, ylims=ylims, fontfamily="monospace")
 	end
 
 	# Plot full KDE
-	Plots.plot!(plt, x, kde, label="KDE", ylims=(0.0, Inf), linewidth=2)
+	Plots.plot!(plt, x, kde, label="KDE", ylims=ylims, linewidth=2, legend=true, fontfamily="monospace")
 end
 
 
-function Plots.scatter(data::MovingGaussianMixture, what=:M; markersize=.5, shade=false, kwargs...)
+function Plots.scatter(
+	data::MovingGaussianMixture, what=:M;
+	title::Union{Missing, AbstractString}=missing, markersize=.5, seriescolor=:deep,
+	shade=false, kwargs...
+	)
 	@assert what ∈ (:M, :Σ) "Don't know how to plot $what"
 	
 	mat = (what == :M) ? data.M : data.Σ
 	
-	title = "Moving gaussian mixture $(repr(data.key))"
+	if title === missing
+		title = "Moving gaussian mixture $(repr(data.key))"
+	end
 	
 	Plots.scatter(
 		data.dates, mat';
 		label="", title=title,
 		markerstrokewidth=0, markerstrokealpha=0, markersize=markersize,
 		marker_z=(shade ? data.P' : nothing),
-		seriescolor=(shade ? :deep : :auto),
+		seriescolor=(shade ? seriescolor : :auto),
 		kwargs...
 	)
 end
