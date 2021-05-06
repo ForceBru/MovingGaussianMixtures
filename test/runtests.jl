a951ed380b72a9dbac65506226a9f4fbfa6cc84c
ForceBru <ForceBru@users.noreply.github.com>
2021-05-06 13:37:59 +0300

    Organized tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using DelimitedFiles
+using Test
+
 using Plots, HDF5
+using BenchmarkTools
 
 using MovingGaussianMixtures
 
@@ -7,66 +10,86 @@ sample_data = readdlm("sample_data.csv")[:, 1]
 
 # Don't automatically show plots
 default(show=false)
-const DISPLAY = false
-
-function do_display(last=false)
-	display(plt)
-
-	if ! last
-		print("Hit ENTER for next plot: ")
-		readline()
-	end
-end
 
 const PLOT_SIZE = (800 * 1.5, 800)
 const N_COMPONENTS = UInt(7)
 const WINDOW_SIZE = UInt(10)
 const OUT_FILE = "sample_results.h5"
 
-@info "Estimating with k-means..."
-ret_kmeans = kmeans(sample_data, N_COMPONENTS)
-lik = log_likelihood(sample_data, ret_kmeans)
-plt = plot(ret_kmeans, sample_data, size=PLOT_SIZE, title="Log-likelihood: $lik")
-savefig(plt, "img/mixture_kmeans.png")
+@testset verbose=true "Basic computations" begin
+	@test let
+		@info "Estimating with k-means..."
+		ret_kmeans = kmeans(sample_data, N_COMPONENTS)
+		lik = log_likelihood(sample_data, ret_kmeans)
+		plt = plot(ret_kmeans, sample_data, size=PLOT_SIZE, title="Log-likelihood: $lik")
+		savefig(plt, "img/mixture_kmeans.png")
+		true
+	end
 
-DISPLAY && do_display()
+	@test let
+		@info "Estimating with EM..."
+		ret_em = em(sample_data, N_COMPONENTS)
+		lik = log_likelihood(sample_data, ret_em)
+		plt = plot(ret_em, sample_data, size=PLOT_SIZE, title="Log-likelihood: $lik")
+		savefig(plt, "img/mixture_em.png")
+		true
+	end
 
-@info "Estimating with EM..."
-ret_em = em(sample_data, N_COMPONENTS)
-lik = log_likelihood(sample_data, ret_em)
-plt = plot(ret_em, sample_data, size=PLOT_SIZE, title="Log-likelihood: $lik")
-savefig(plt, "img/mixture_em.png")
+	@test let
+		@info "Running MGM (kmeans)..."
+		ret_mov_kmeans = moving_kmeans(sample_data, N_COMPONENTS, WINDOW_SIZE, step_size=UInt(1))
+		plt = scatter(ret_mov_kmeans, markersize=4, alpha=.5, size=PLOT_SIZE)
+		savefig(plt, "img/running_kmeans.png")
+		true
+	end
 
-DISPLAY && do_display()
-
-@info "Running MGM (kmeans)..."
-ret_mov_kmeans = moving_kmeans(sample_data, N_COMPONENTS, WINDOW_SIZE, step_size=UInt(1))
-plt = scatter(ret_mov_kmeans, markersize=4, alpha=.5, size=PLOT_SIZE)
-savefig(plt, "img/running_kmeans.png")
-
-@info "Running MGM (EM)..."
-ret_mov_em = moving_em(sample_data, N_COMPONENTS, WINDOW_SIZE, step_size=UInt(1))
-plt = scatter(ret_mov_em, markersize=4, alpha=.5, size=PLOT_SIZE)
-savefig(plt, "img/running_em.png")
-
-@info "Saving moving EM data to $OUT_FILE..."
-h5open(OUT_FILE, "w") do fid
-	write(fid, ret_mov_em)
+	@test let
+		@info "Running MGM (EM)..."
+		ret_mov_em = moving_em(sample_data, N_COMPONENTS, WINDOW_SIZE, step_size=UInt(1))
+		plt = scatter(ret_mov_em, markersize=4, alpha=.5, size=PLOT_SIZE)
+		savefig(plt, "img/running_em.png")
+		true
+	end
 end
-@info "Data saved!"
 
-@info "Reading data from $OUT_FILE..."
-ret_read = h5open(OUT_FILE, "r") do fid
-	read(fid, MovingGaussianMixtures.MovingGaussianMixture, parse_dates=false)
+@testset verbose=true "Saving/reading" begin
+	@test let
+		ret_mov_em = moving_em(sample_data, N_COMPONENTS, WINDOW_SIZE, step_size=UInt(1))
+		@info "Saving moving EM data to $OUT_FILE..."
+		h5open(OUT_FILE, "w") do fid
+			write(fid, ret_mov_em)
+		end
+		true
+	end
+
+	@test begin
+		@info "Reading data from $OUT_FILE..."
+		ret_read = h5open(OUT_FILE, "r") do fid
+			read(fid, MovingGaussianMixtures.MovingGaussianMixture, parse_dates=false)
+		end
+		true
+	end
+
+	@test let
+		@info "Plotting data from $OUT_FILE..."
+		ret_read = h5open(OUT_FILE, "r") do fid
+			read(fid, MovingGaussianMixtures.MovingGaussianMixture, parse_dates=false)
+		end
+		ret_mov_kmeans = moving_kmeans(sample_data, N_COMPONENTS, WINDOW_SIZE, step_size=UInt(1))
+		plt_em = scatter(ret_read, markersize=2, alpha=.8, shade=true)
+		plt_kmeans = scatter(ret_mov_kmeans, markersize=2, alpha=.8, shade=true)
+		savefig(plot(plt_em, plt_kmeans, size=PLOT_SIZE), "img/running_shaded.png")
+		true
+	end
 end
-@info "Data read!"
 
-@info "Plotting data from $OUT_FILE..."
-plt_em = scatter(ret_read, markersize=2, alpha=.8, shade=true)
-plt_kmeans = scatter(ret_mov_kmeans, markersize=2, alpha=.8, shade=true)
-savefig(plot(plt_em, plt_kmeans, size=PLOT_SIZE), "img/running_shaded.png")
-
-DISPLAY && do_display(true)
-
-@info "ALL DONE!"
-
+@testset verbose=true "Benchmarks" begin
+	@test let
+		@info "Benchmarking EM..."
+		data = GaussianMixture(sample_data, UInt(20))
+		b_data = @benchmark em!($data, $sample_data)
+		show(stdout, "text/plain", b_data)
+		println()
+		true
+	end
+end
