f6e072c16605667c35a909711ce02831e20b2a77
ForceBru <ForceBru@users.noreply.github.com>
2021-07-16 00:26:42 +0300

    Hopefully faster saving to DB

--- src/SaveSQL.jl
+++ src/SaveSQL.jl
@@ -39,13 +39,35 @@ function save_sql(
 
     win_size = params.range[1]
     stmt = DBInterface.prepare(conn, "INSERT INTO $table_name VALUES (:id, :date, :win_size, :component_no, :p, :μ, :σ)")
+
+    TOTAL_SIZE = length(params.range) * params.K
+
+    all_ids = fill(id, TOTAL_SIZE)
+    all_dates = eltype(dates)[]
+    all_win_sizes = fill(win_size, TOTAL_SIZE)
+    all_k = repeat(1:params.K, length(params.range))
+    all_p = zeros(T, TOTAL_SIZE)
+    all_μ = zeros(T, TOTAL_SIZE)
+    all_σ = zeros(T, TOTAL_SIZE)
     
+    idx = 1
     for (i, t) ∈ enumerate(params.range)
         for k ∈ 1:params.K
             p, μ, σ = @inbounds (params.P[k, i], params.M[k, i], params.Σ[k, i])
-            DBInterface.execute(stmt, [id, dates[t], win_size, k, p, μ, σ])
+
+            push!(all_dates, dates[t])
+            all_p[idx] = p
+            all_μ[idx] = μ
+            all_σ[idx] = σ
+
+            idx += 1
         end
     end
+
+    DBInterface.executemany(stmt, (
+        id=all_ids, date=all_dates, win_size=all_win_sizes,
+        component_no=all_k, p=all_p, μ=all_μ, σ=all_σ
+    ))
     
     DBInterface.close!(stmt)
 end
