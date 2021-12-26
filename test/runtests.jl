c45de08570af115458790a9ccbaa26f063890cb6
ForceBru <ForceBru@users.noreply.github.com>
2021-12-26 17:11:12 +0300

    Split tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,6 +13,12 @@ function sample_GMM(mus, stds, ps)
     distr, data
 end
 
+function isapprox_sorted(x::AbstractVector, y::AbstractVector; atol::Real)::Bool
+    @assert atol > 0
+
+    isapprox(sort(x), sort(y); atol)
+end
+
 @testset "Simple mixtures" begin
     @testset "2 components easy" begin
         distr, data = sample_GMM([-1, 1], [.2, .3], [.4, .6])
@@ -21,9 +27,9 @@ end
         fit!(gmm, data)
 
         @test gmm.converged
-        @test isapprox(sort(gmm.p), sort(distr.prior.p); atol)
-        @test isapprox(sort(gmm.mu), sort(distr.means); atol)
-        @test isapprox(sort(gmm.var), sort(distr.stds.^2); atol)
+        @test isapprox_sorted(gmm.p, distr.prior.p; atol)
+        @test isapprox_sorted(gmm.mu, distr.means; atol)
+        @test isapprox_sorted(gmm.var, distr.stds.^2; atol)
     end
 
     @testset "2 components zero means" begin
@@ -33,11 +39,13 @@ end
         fit!(gmm, data)
 
         @test gmm.converged
-        @test isapprox(sort(gmm.p), sort(distr.prior.p); atol)
-        @test isapprox(sort(gmm.mu), sort(distr.means); atol)
-        @test isapprox(sort(gmm.var), sort(distr.stds.^2); atol)
+        @test isapprox_sorted(gmm.p, distr.prior.p; atol)
+        @test isapprox_sorted(gmm.mu, distr.means; atol)
+        @test isapprox_sorted(gmm.var, distr.stds.^2; atol)
     end
+end
 
+@testset "Regularization" begin
     @testset "Variance reg by addition" begin
         REG = 1e-4
         distr, data = sample_GMM([0, 0], [.2, .3], [.4, .6])
