cd5d7cba78ab5ea376872a5066a494f0b2650ed8
ForceBru <ForceBru@users.noreply.github.com>
2021-12-30 22:04:22 +0300

    feat: tests for moving mixtures

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,7 +15,7 @@ end
 
 @testset verbose=true "MovingMixtures" begin
 
-@testset "Simple mixtures" begin
+@testset verbose=true "Simple mixtures" begin
     @testset "2 components easy $i" for i in 1:10
         distr, data = sample_GMM([-1, 1], [.2, .3], [.4, .6])
 
@@ -42,7 +42,7 @@ end
     end
 end
 
-@testset "Regularization" begin
+@testset verbose=true "Regularization" begin
     @testset "Variance reg by addition $i" for i in 1:10
         REG = 1e-4
         distr, data = sample_GMM([0, 0], [.2, .3], [.4, .6])
@@ -63,7 +63,23 @@ end
     end
 end
 
-@testset "Divergences" begin
+@testset "Dynamics" begin
+    distr, data = sample_GMM([-1, 1], [.2, .3], [.4, .6])
+    p_sorted = sort(distr.prior.p)
+    mu_sorted = sort(distr.means)
+    var_sorted = sort(distr.stds .^2)
+
+    dmm = DynamicMixture(Online(GaussianMixture(2), 0.9))
+    fit!(dmm, data, 200)
+
+    @testset "Check params $t" for t in 1:size(dmm.P, 2)
+        @test sort(dmm.P[:, t]) ≈ p_sorted atol=atol
+        @test sort(dmm.M[:, t]) ≈ mu_sorted atol=atol
+        @test sort(dmm.V[:, t]) ≈ var_sorted atol=atol
+    end
+end
+
+@testset verbose=true "Divergences" begin
     @testset "Cauchy-Schwarz $i" for i in 1:10
         distr, data = sample_GMM([-rand(), rand()], [.2, .3], [.4, .6])
 
@@ -72,7 +88,7 @@ end
         distr_hat = distribution(gmm)
 
         @test cauchy_schwarz(distr, distr) ≥ 0
-        @test cauchy_schwarz(distr, distr) ≈ 0
+        @test cauchy_schwarz(distr, distr) ≈ 0 atol=1e-10
         @test cauchy_schwarz(distr, distr_hat) ≈ cauchy_schwarz(distr_hat, distr)
         @test cauchy_schwarz(distr, distr_hat) ≈ 0 atol=atol
     end
